ce51a75f61400dad58a24c31bbcb93f1a64fcb00
ol-biaa <olbiaaletta@gmail.com>
2024-05-13 22:48:18 -0700

    validations

--- udemy-react/src/Pages/Login/Login.js
+++ udemy-react/src/Pages/Login/Login.js
@@ -1,38 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './login.css'; // Import CSS file for login styles
 import Header from '../../Componets/Header/Header';
 import Footer from '../../Componets/Footer/Footer';
+import { Link } from 'react-router-dom';
+import Validation from './LoginValidation'; // Corrected import statement
 
-const Login = () => {
-    const checkUser = (event) => {
-        // Your login logic here
+function Login(){
+    const [values, setValues] = useState({
+        email: '',
+        password: ''
+    });
+    const [errors, setErrors] = useState([]);
+
+    const handleInput = (event) => {
+        setValues(prev => ({...prev, [event.target.name]: event.target.value})); // Fixed setting value as string, not array
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setErrors(Validation(values)); // Corrected function call to Validation(values)
     };
 
     return (
         <div>
-        <Header />
-        <div id="top">
-            <div id="sign_up_page">
-                <h2>Log In to Your Udemy Account!</h2>
-                <div className="paragraph"></div>
+            <Header />
+            <br />  <br />  <br />
+            <div id="top">
+                <div id="sign_up_page">
+                    <h2>Log In to Your Udemy Account!</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className="paragraph"></div>
 
-                <input id="email" className="name" type="email" placeholder="Email" />
-                <input id="pass" className="name" type="password" placeholder="Password" />
+                        <input id="email"
+                            className="name" type="email" placeholder="Email" onChange={handleInput} name='email' />
 
-                <button id="sign-btn" onClick={checkUser} className="sign">Log In</button>
+                        <span>{errors.email && <span className='text-danger'> {errors.email}</span>}</span> {/* Added missing closing span tag */}
 
-                <div className="already">
-                    <h4>OR</h4>
-                    <h4><a className="color_1" href="signup.html">Forgot Password</a></h4>                </div>
+                        <input id="pass" className="name" type="password" placeholder="Password" onChange={handleInput} name='password' />
 
-                {/* <div></div> */}
-                <div className="already">
-                    <h4>Don't have an account?</h4>
-                    <h4><a className="color_1" href="signup.html">Sign Up</a></h4>
+                        <span>{errors.password && <span className='text-danger'> {errors.password}</span>}</span> {/* Added missing closing span tag */}
+
+                        <button type='submit' id="sign-btn" className="sign">Log In</button>
+
+                        <div className="already">
+                            <h3>OR</h3>
+                            <Link to="/forgot" className="color_1" >Forgot Password</Link>
+                        </div>
+
+                        <div className="already">
+                            <h4>Don't have an account?</h4> 
+                            <Link to="/signup" className="color_1">Sign Up</Link>
+                        </div>
+                    </form>
                 </div>
             </div>
-        </div>
-        <Footer />
+            <Footer />
         </div>
     );
 };
--- udemy-react/src/Pages/Signup/Signup.js
+++ udemy-react/src/Pages/Signup/Signup.js
@@ -1,38 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './signup.css'; // Import CSS file for Fup styles
 import Footer from '../../Componets/Footer/Footer';
 import Header from '../../Componets/Header/Header';
+import { Link, useNavigate } from 'react-router-dom';
+import Validation from './SignupValidation';
+import axios from 'axios';
 
 
-const Signup = () => {
-    const signupform = (event) => {
-        // Your signup form logic here
+function Signup(){
+    const [values, setValues] = useState({
+        name:'',
+        email: '',
+        password: ''
+    });
+
+    const navigate = useNavigate();
+    const [errors, setErrors] = useState([]);
+
+    const handleInput = (event) => {
+        setValues(prev => ({...prev, [event.target.name]: event.target.value})); // Fixed setting value as string, not array
     };
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const err = Validation(values);
+        setErrors(err);
+        if (err.name === "" && err.email === "" && err.password === "") {
+            axios.post('http://localhost:8081/signup', values)
+                .then(res => {
+                    navigate('/login');
+                })
+                .catch(error => console.log(error)); // Change err to error and log error object
+        }
+    };
+    
 
     return (
         <div>
          <Header />
         <div id="sign_up_page">
-        
+        <br />  <br />  <br />
             <h2>Sign Up and Start Learning! </h2>
-            <div></div>
+            <form action='' onSubmit={handleSubmit}>
         
-            <input id="name" className="name" type="text" placeholder="Full Name" />
-            
-            <input id="email" className="name" type="email" placeholder="Email" />
-        
-            <input id="pass" className="name" type="password" placeholder="Password" />
-            <div id="alert"></div>
+            <input id="name" className="name" type="text" name="name" placeholder="Full Name" onChange={handleInput} />
+
+            <span>{errors.name && <span className='text-danger'> {errors.name}</span>}</span> {/* Added missing closing span tag */}
+
+<input id="email" className="name" type="email" placeholder="Email" name="email" onChange={handleInput} />
+
+<span>{errors.email && <span className='text-danger'> {errors.email}</span>}</span> {/* Added missing closing span tag */}
+
+<input id="pass" className="name" type="password" placeholder="Password" name="password" onChange={handleInput} />
+
+<span>{errors.password && <span className='text-danger'> {errors.password}</span>}</span> {/* Added missing closing span tag */}
+
             <br />
-            <button id="sign-btn" onClick={signupform} className="sign">Sign Up</button>
+            <button type='submit' id="sign-btn" className="sign">Sign Up</button>
         
         <p>By signing up, you agree to our Terms of Use and Privacy Policy.</p>
         <div></div>
         <div className="already">
             <h3>Already have an account?</h3>
-            <h3><a className="color_1" href="login.html">Log In</a></h3> 
-        </div> 
-       
+            <Link to="/" className="color_1">Log In</Link>        </div> 
+            </form>
     </div>
     <br />  <br />  <br />
       <Footer /> 
